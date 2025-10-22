a30062727592dacc31e13b6b2a5abd127738fa2e
Karam <72949304+ooMaybe@users.noreply.github.com>
2025-10-22 14:17:22 -0700

    v1.0.0
    Introduces main.cpp with a simple entry point that prints a welcome message for the Untitled Music Playlist project.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*****************************************************************************\
+    Class: main.cpp
+    Author: Karam, Alex
+    Date: ???
+    Description: This class currently serves as the entry point for 
+        the Untitled Music Playlist project.
+\*****************************************************************************/
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to the Untitled Music Playlist!" << std::endl;
+    return 0;
+}
